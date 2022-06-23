8aff297bd8e59042c30b757301d74a5fdeeb8cc7
neeraj162 <ashishneeraj2003@gmail.com>
2022-06-23 23:28:11 +0530

    Final class based

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -13,7 +13,6 @@ export class Newsitem extends Component {
                         {source}
                     </span>
                     </div>
-                    
                     <div className="row g-0">
                         <div className="col-md-4">
                             <img src={!imgUrl ? "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" : imgUrl} className="img-fluid rounded-start" alt="..." />
